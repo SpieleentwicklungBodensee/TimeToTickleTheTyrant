1866540354848afe1452636d2dec081ffdb33d7f
Max Bachmann <oss@maxbachmann.de>
2024-01-26 22:53:25 +0100

    fix broken merge

--- Fluid.py
+++ Fluid.py
@@ -4,3 +4,7 @@ class Fluid:
     def __init__(self, numX, numY):
         self.velocity = np.zeros(shape=(numX, numY, 2))
         self.space = np.zeros(shape=(numX, numY))
+
+        for y in range(self.lev_h):
+            for x in range(self.lev_w):
+                self.space[x, y] = 0.0 if self.level[y][x] == '#' else 1.0
